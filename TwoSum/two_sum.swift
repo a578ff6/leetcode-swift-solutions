b7a916840e44ba06712e06444b9bdd84e08fbe56
a578ff6 <a578ff6@gmail.com>
2025-03-26 15:52:33 +0800

    Update: 加入 Two Sum 解法二（HashMap）與筆記補充

--- TwoSum/two_sum.swift
+++ TwoSum/two_sum.swift
@@ -1,5 +1,6 @@
 // LeetCode #1 - Two Sum
 
+// 解法一：暴力法
 func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
     for i in 0..<nums.count {
         for j in i+1..<nums.count {
@@ -8,5 +9,20 @@ func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
             }
         }
     }
+    return []
+}
+
+// 解法二：使用 Dictionary 優化
+func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+    var dict = [Int: Int]()
+    
+    for (i, num) in nums.enumerated() {
+        let complement = target - num
+        if let j = dict[complement] {
+            return [j, i]
+        }
+        dict[num] = i
+    }
+    
     return []
 }
